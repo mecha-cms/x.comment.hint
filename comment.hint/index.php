e1cd2c51a3b8c9f4d266b949ccf481352fc4e833
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-03-10 17:02:29 +0700

    Update

--- comment.hint/index.php
+++ comment.hint/index.php
@@ -1,16 +1,31 @@
-<?php namespace _\lot\x\comment;
+<?php namespace x;
 
-function hint($content) {
-    if (false !== ($i = \strpos($content, ' name="comment[content]"'))) {
+function comment__hint($content) {
+    foreach (['author', 'email', 'link'] as $v) {
+        if (false !== ($i = $test = \strpos($content, ' name="comment[' . $v . ']"'))) {
+            $end = '>';
+            if (false !== ($j = \strpos(\substr($content, $i), $end))) {
+                $i += $j + \strlen($end);
+                if (false === \strpos(\substr($content, $test, $j), ' type="hidden"')) {
+                    $hint = \State::get("x.comment\\.hint." . $v);
+                    $hint = $hint ? '<br><small>' . \i($hint) . '</small>' : "";
+                    $content = \substr($content, 0, $i) . $hint . \substr($content, $i);
+                }
+            }
+        }
+    }
+    if (false !== ($i = $test = \strpos($content, ' name="comment[content]"'))) {
         $end = '</textarea>';
         if (false !== ($j = \strpos(\substr($content, $i), $end))) {
             $i += $j + \strlen($end);
-            $hint = \State::get("x.comment\\.hint.hint");
-            $hint = $hint ? '<br><small>' . \i($hint) . '</small>' : "";
-            return \substr($content, 0, $i) . $hint . \substr($content, $i);
+            if (false === \strpos(\substr($content, $test, $j), ' type="hidden"')) {
+                $hint = \State::get("x.comment\\.hint.content");
+                $hint = $hint ? '<br><small>' . \i($hint) . '</small>' : "";
+                $content = \substr($content, 0, $i) . $hint . \substr($content, $i);
+            }
         }
     }
     return $content;
 }
 
-\Hook::set('content', __NAMESPACE__ . "\\hint", 10);
+\Hook::set('content', __NAMESPACE__ . "\\comment__hint", 10);
--- comment.hint/state.php
+++ comment.hint/state.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'hint' => 'Inline HTML tags such as <code>&lt;a&gt;</code>, <code>&lt;code&gt;</code>, <code>&lt;em&gt;</code> and <code>&lt;strong&gt;</code> are allowed in the comment body.'
+    // 'author' => 'Please use your real name.',
+    // 'email' => 'We use this email address to find your profile picture which comes from the <a href="https://gravatar.com" rel="nofollow" target="_blank">Gravatar</a> service. Your email address will remain hidden.',
+    // 'link' => 'Optional. Please use the home page URL.',
+    'content' => 'Inline HTML tags such as <code>&lt;a&gt;</code>, <code>&lt;code&gt;</code>, <code>&lt;em&gt;</code> and <code>&lt;strong&gt;</code> are allowed in the comment body.'
 ];
